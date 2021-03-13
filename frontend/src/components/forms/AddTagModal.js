4e27b4b89bb311514f417a1b09ba8256debb27ef
Gandifil <Gandifilmk@gmail.com>
2021-03-13 04:35:54 +0300

    Fix modal for adding tag.

--- frontend/src/components/forms/AddTagModal.js
+++ frontend/src/components/forms/AddTagModal.js
@@ -1,21 +1,38 @@
 ﻿import React, { Component } from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Label, Input, FormGroup } from 'reactstrap';
+import { ReactAutocomplete } from 'react-autocomplete'
 
 export class AddTagModal extends Component {
     constructor(props) {
         super(props);
         this.state = {
-             title: "",
+            index: -1,
+            tags: [],
             visible: false,
+            loading: true,
         }
 
-        this.handleTitle = (e) => this.setTitle(e.target.value);
+        this.handleIndex = (e) => this.setIndex(e.target.value);
         this.handleNo = (e) => this.hide();
-        this.handleYes = (e) => submit();
+        this.handleYes = (e) => this.submit();
     }
 
-    setTitle(value) {
-        this.state.title = value;
+    componentDidMount() {
+        this.state.loading = true;
+        this.setState(this.state);
+        const url = 'api/tags';
+        fetch(url)
+            .then(response => response.json())
+            .then(result => this.setState({ tags: result, loading: false, visible: true }))
+            .catch(e => {
+                console.log(e);
+                this.setState({tags: [], visible: true, loading: false, error: e });
+            });
+    }
+
+
+    setIndex(value) {
+        this.state.index = value;
         this.setState(this.state);
     }
 
@@ -31,18 +48,24 @@ export class AddTagModal extends Component {
 
     submit() { 
         this.hide();
-        console.log("Установлен фильтр", this.state.item);
+        console.log("Установлен тег с ID", this.state.index);
+
+    }
+
+    renderTag(tag) {
+        return (<option value={tag.id} >{ tag.name }</option>)
     }
 
     render() {
         return (
 <Modal isOpen={this.state.visible} >
     <ModalHeader>Добавить тег</ModalHeader>
-    <ModalBody>
-        <FormGroup>
-                        <Label>Текст</Label>
-                        <Input name="title" value={this.state.title} onChange={this.handleTitle} />
-        </FormGroup>
+                <ModalBody>
+                    <Input type="select" bsSize="lg" onChange={this.handleIndex}>
+            <option selected>Выберете тег</option>
+                        {this.state.tags.map(x => this.renderTag(x))}
+            
+            </Input>
     </ModalBody>
                 <ModalFooter>
                     <Button color="secondary" onClick={this.handleNo}>Отмена</Button>
