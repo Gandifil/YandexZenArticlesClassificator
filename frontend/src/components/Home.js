f91c179af91c8ca124006fd7a088ef134a8a0084
Gandifil <Gandifilmk@gmail.com>
2021-03-13 12:17:48 +0300

    Fix home up.

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,21 +3,32 @@ import React, { Component } from 'react';
 export class Home extends Component {
   render () {
     return (
-      <div>
-        <h1>Hel, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
+        <div>
+            <h1>YandexZenArticlesClassificator</h1>
+
+            <h2>Описание системы</h2>
+
+
+            <p>YandexZenArticlesClassificator – сервис для классификации
+текстов на основе собранных статей с сайта Yandex Zen. Кроме того, он включает возможность просмотра и
+редактирования хранилища статей. </p>
+
+            <p>Условно сервис можно разделить на 3 части:</p>
+
+            <p>Функциональность классификации. Пользователю
+предлагается ввести или загрузить из файла некий текст, который будет
+проанализирован. После этого система выдает пользователю информацию о том, к
+какой категории принадлежит текст.</p>
+
+            <p>Функциональность поиска по хранилищу статей. Пользователь
+имеет возможность получить список статей, которые подходят по введенным данным.
+В странице результата поиска пользователь может выбрать галочками статьи и
+удалить их. </p>
+
+            <p>Функциональность просмотра статей,
+редактирования, удаления, создания новых. При редактировании пользователь может
+изменить любую составляющую статьи(название, автора, тег и т.д.).</p>
+
       </div>
     );
   }
